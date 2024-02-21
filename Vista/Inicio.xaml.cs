7af63af75af5542be2700202f074edd1f2b2c449
YesicaYMR <yesica.macho@gmail.com>
2024-02-21 18:52:49 +0100

    Agregar archivos de proyecto.

---
+++ Vista/Inicio.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrimeraAplicacionWPF.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        public void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton==MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnMinimizar_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+           
+            SqlConnection connection = new SqlConnection("Data Source = 172.26.4.166; Initial Catalog = DBVENTAS; User ID = sa; Password = 87654321");
+            connection.Open();
+            SqlCommand query = new SqlCommand("select idUsuario from USUARIO where Documento = @documento and Clave = @clave", connection);
+            
+            var documento = txtUser.Text;
+            var clave = pwdContra.Password.ToString();
+
+            
+            query.Parameters.AddWithValue("@documento", documento);
+            query.Parameters.AddWithValue("@clave", clave);
+
+            
+            var reader = query.ExecuteReader();
+
+           
+            if (reader.Read())
+            {
+                this.Hide();
+                YesicaMacho yesicaMacho = new YesicaMacho();
+                yesicaMacho.Show();;
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            connection.Close();
+
+
+        }
+
+
+    }
+}
---
+++ YesicaMacho.xaml.cs
@@ -0,0 +1,69 @@
+﻿using PrimeraAplicacionWPF.Vista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PrimeraAplicacionWPF
+{
+    /// <summary>
+    /// Lógica de interacción para YesicaMacho.xaml
+    /// </summary>
+    public partial class YesicaMacho : Window
+    {
+        public YesicaMacho()
+        {
+            InitializeComponent();
+            btnMostrar.Visibility = Visibility.Collapsed;
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            Window1 window1 = new Window1();
+            window1.Show();
+           
+        }
+
+        private void btnEsconder_Click(object sender, RoutedEventArgs e)
+        {
+            menu.Visibility = Visibility.Collapsed;
+            btnMostrar.Visibility = Visibility.Visible;
+
+        }
+
+        private void resuelto_Checked(object sender, RoutedEventArgs e)
+        {
+            if(resuelto.IsChecked == true)
+            {
+                enCurso.IsChecked = false;
+            }
+        }
+
+        private void enCurso_Checked(object sender, RoutedEventArgs e)
+        {
+            if (enCurso.IsChecked == true)
+            {
+                resuelto.IsChecked = false;
+            }
+        }
+
+        private void btnMostrar_Click(object sender, RoutedEventArgs e)
+        {
+            menu.Visibility = Visibility.Visible;
+            btnMostrar.Visibility= Visibility.Collapsed;
+        }
+
+        
+    }
+}
